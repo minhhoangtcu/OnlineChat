82c16c1dcbd749a13cf547c834ac136d760567ed
minhhoangtcu <minh.hoang@tcu.edu>
2015-04-14 16:03:18 -0500

    Added basic features to View

--- OnlineChat/src/View.java
+++ OnlineChat/src/View.java
@@ -4,11 +4,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 
 public class View extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -34,8 +38,25 @@ public class View extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 11, 414, 221);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.setBounds(10, 238, 89, 23);
+		contentPane.add(btnSend);
+		
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.setBounds(104, 238, 89, 23);
+		contentPane.add(btnConnect);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(203, 239, 221, 22);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
 	}
-
 }
