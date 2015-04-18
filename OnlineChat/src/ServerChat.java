bbf727acdc248fcb582330a7c6db79f0b7f339cd
minhhoangtcu <minh.hoang@tcu.edu>
2015-04-18 10:58:44 -0500

    Added a working server

--- OnlineChat/src/ServerChat.java
+++ OnlineChat/src/ServerChat.java
@@ -18,7 +18,7 @@ public class ServerChat {
 		currentNumberOfClients = 0;
 		try {
 			server = new ServerSocket(PORT);
-			System.out.print("Server initiated");
+			System.out.println("Server initiated");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -42,10 +42,18 @@ public class ServerChat {
 	public void startClients() throws IOException{
 		 while (true) {
 			 Socket client = server.accept();
-			 System.out.print("Client connected");
-			 ServiceChat service = new ServiceChat(client);
+			 System.out.println("Client connected");
+			 ServiceChat service = new ServiceChat(client, this, currentNumberOfClients);
+			 services[currentNumberOfClients] = service;
+			 currentNumberOfClients++;
 			 Thread t = new Thread(service);
 			 t.start();
 		 }
+	}
+	
+	public void printAll(String input) {
+		for (int i = 0; i < currentNumberOfClients; i++) {
+			services[i].print(input);
+		}
 	}
 }
