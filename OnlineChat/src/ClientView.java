6880f4e0852090b40b9fc511c8f1ef977a935bb0
minhhoangtcu <minh.hoang@tcu.edu>
2015-04-20 16:30:13 -0500

    Moved all GUI elements of Client.class into a new class.

--- OnlineChat/src/ClientView.java
+++ OnlineChat/src/ClientView.java
@@ -1,57 +1,127 @@
-import java.awt.BorderLayout;
-import java.awt.event.*;
-import java.awt.EventQueue;
-import java.awt.event.ActionListener;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.border.EmptyBorder;
 import javax.swing.*;
-
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.io.*;
+import java.util.*;
 
-public class ClientView extends JPanel {
-	private JTextField tfSever;
-	private JTextField tfPort;
+public class ClientView extends JFrame{
+	JTextArea result = new JTextArea();
+	JTextField userInput = new JTextField(20);
+	JTextField tfSever = new JTextField();
+	JTextField tfPort = new JTextField();
+	JButton connectButton = new JButton("Connect");
+	JButton sendButton = new JButton("Send");
+	JLabel errors = new JLabel();
+	JLabel lbSever = new JLabel("Sever");
+	JLabel lblPort = new JLabel("Port");
+	JScrollPane scroller = new JScrollPane();
+	Color purple = new Color (153, 0, 204);
+	JPanel contentPane;
+	
+	public static void main() {
+		ClientView view = new ClientView();
+		view.setVisible(true);
+	}
+	
 	public ClientView() {
-		setLayout(null);
+		buildView();
+	}
+	
+	private void buildView() {
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 490, 375);
+		setResizable(false);
+		setUpContentPane();
+		setUpResult();
+		setUpUserInput();
+		setUpSendButton();
+		setUpConnectButton();
+		setUpServerLabel();
+		setUpServerTF();
+		setUpPortLabel();
+		setUpPortTF();
+		setUpError();
+	}
+	
+	private void setUpContentPane() {
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setOpaque(true);
+		contentPane.setBackground(new Color(250, 250, 250));
+		contentPane.setLayout(null);
+	}
+
+	private void setUpResult() {
+		result.setBorder(BorderFactory.createLineBorder(purple,1));
+		result.addMouseListener(new java.awt.event.MouseAdapter() {
+		    public void mouseEntered(java.awt.event.MouseEvent evt) {
+		        result.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+		    }
+
+		    public void mouseExited(java.awt.event.MouseEvent evt) {
+		        result.setBorder(BorderFactory.createLineBorder(purple,1));
+		    }
+		});
+		scroller.getViewport().add(result);
+		scroller.setBounds(10, 11, 344, 258);
+		contentPane.add(add(scroller));
+	}
 		
-		JTextArea result = new JTextArea();
-		result.setForeground(Color.BLACK);
-		result.setBounds(10, 11, 344, 258);
-		add(result);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(10, 280, 344, 24);
-		add(textArea);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(364, 281, 113, 23);
-		add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(364, 310, 113, 23);
-		add(btnNewButton_1);
-		
-		JLabel lbSever = new JLabel("Sever");
-		lbSever.setFont(new Font("SansSerif", Font.PLAIN, 12));
+	private void setUpUserInput() {
+		contentPane.add(userInput); 
+		userInput.setBounds(10, 280, 344, 24);
+		userInput.setColumns(10);
+		userInput.setBorder(BorderFactory.createLineBorder(purple,1));
+	}
+	
+	private void setUpSendButton() {
+		contentPane.add(sendButton); 
+		sendButton.setBounds(364, 281, 113, 23);
+		sendButton.setEnabled(false);
+		sendButton.setContentAreaFilled(false);
+		sendButton.setBackground(purple);
+		sendButton.setForeground(Color.WHITE);
+	}
+			
+	private void setUpConnectButton() {
+		contentPane.add(connectButton); 
+		connectButton.setBounds(364, 310, 113, 23);
+		connectButton.setBackground(purple);
+		connectButton.setForeground(Color.WHITE);
+	}
+
+	private void setUpServerLabel() {
+		lbSever.setFont(new Font("SansSerif", Font.BOLD, 12));
 		lbSever.setBounds(10, 315, 50, 14);
-		add(lbSever);
-		
-		tfSever = new JTextField();
+		lbSever.setForeground(purple);
+		contentPane.add(lbSever);
+	}
+	
+	private void setUpServerTF() {
 		tfSever.setBounds(55, 311, 137, 20);
-		add(tfSever);
 		tfSever.setColumns(10);
+		tfSever.setBorder(BorderFactory.createLineBorder(purple,1));
+		tfSever.setText("localhost");
+		contentPane.add(tfSever);
+	}
 		
-		tfPort = new JTextField();
-		tfPort.setBounds(268, 311, 86, 20);
-		add(tfPort);
-		tfPort.setColumns(10);
-		
-		JLabel lblPort = new JLabel("Port");
-		lblPort.setFont(new Font("SansSerif", Font.PLAIN, 12));
+	private void setUpPortLabel() {
+		lblPort.setFont(new Font("SansSerif", Font.BOLD, 12));
 		lblPort.setBounds(230, 313, 46, 14);
-		add(lblPort);
+		lblPort.setForeground(purple);
+		contentPane.add(lblPort);
+	}
+	
+	private void setUpPortTF() {
+		tfPort.setBounds(268, 311, 86, 20);
+		tfPort.setColumns(10);
+		tfPort.setBorder(BorderFactory.createLineBorder(purple,1));
+		tfPort.setText("6969");
+		contentPane.add(tfPort);
+	}
+	
+	private void setUpError() {
+		contentPane.add(errors);
 	}
 }
