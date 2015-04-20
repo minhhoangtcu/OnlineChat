2b57bea817fd502dfe2e7accb0525a624ad7106a
antonxuanquang <antonxuanquang@yahoo.com>
2015-04-20 00:55:43 -0500

    Using SimpleTelnet as for client side

--- OnlineChat/src/Client.java
+++ OnlineChat/src/Client.java
@@ -1,70 +1,72 @@
 import javax.swing.*;
-
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
 import java.net.*;
 
-public class Client extends JFrame {
-	JPanel contentPane;
+public class Client extends JFrame implements ActionListener {
+	JTextArea result = new JTextArea(20,40);
+	JTextField userInput = new JTextField(20);
+	JButton connectButton = new JButton("Connect");
+	JButton sendButton = new JButton("Send");
+	JLabel errors = new JLabel();
+	JScrollPane scroller = new JScrollPane();
 	Socket socket;
-	BufferedReader in;
+	Scanner in;
 	PrintWriter out;
 	Thread thread;
-	ClientView clientView;
 	
 	public Client() {
-		setResizable(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		clientView = new ClientView(this);
-		add(clientView);
+		setLayout(new java.awt.FlowLayout());
+		setSize(500,430);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		scroller.getViewport().add(result);
+		add(scroller);
+		add(userInput); userInput.addActionListener(this);
+		add(sendButton); sendButton.addActionListener(this); sendButton.setEnabled(false);
+		add(connectButton); connectButton.addActionListener(this);
+		add(errors);
 	}
 	
-	public void connect() {
+	public void actionPerformed(ActionEvent evt) {
 		try {
-			Scanner scanner = new Scanner(clientView.userInput.getText());
-			if (!scanner.hasNext()) return;
-			String host = scanner.next();
-			if (!scanner.hasNextInt()) return;
-			int port = scanner.nextInt();
-			socket = new Socket(host, port);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new PrintWriter(socket.getOutputStream(), false);
-			thread = new ReadThread(in, clientView.result);
-			thread.start();
-			clientView.sendButton.setEnabled(true);
-			clientView.connectButton.setText("Disconnect");
-			clientView.userInput.setText("");
+			if (evt.getActionCommand().equals("Connect") || 
+					connectButton.getText().equals("Connect") && evt.getSource() == userInput) {
+				Scanner scanner = new Scanner(userInput.getText());
+				if (!scanner.hasNext()) return;
+				String host = scanner.next();
+				if (!scanner.hasNextInt()) return;
+				int port = scanner.nextInt();
+				socket = new Socket(host, port);
+				in = new Scanner(socket.getInputStream());
+				out = new PrintWriter(socket.getOutputStream(), false);
+				thread = new ReadThread(in, result);
+				thread.start();
+				sendButton.setEnabled(true);
+				connectButton.setText("Disconnect");
+				userInput.setText("");
+			}
+			else if (evt.getActionCommand().equals("Disconnect")) {
+				thread.interrupt();
+				socket.close();
+				in.close();
+				out.close();
+				sendButton.setEnabled(false);
+				connectButton.setText("Connect");
+			}
+			else if (evt.getActionCommand().equals("Send") || 
+						sendButton.isEnabled() && evt.getSource() == userInput) {
+				out.print(userInput.getText() + "\n");
+				out.flush();
+				result.append("> " + userInput.getText() + '\n');
+				userInput.setText("");
+			}
 		} catch(UnknownHostException uhe) {
-			clientView.errors.setText(uhe.getMessage());
+			errors.setText(uhe.getMessage());
 		} catch(IOException ioe) {
-			clientView.errors.setText(ioe.getMessage());
+			errors.setText(ioe.getMessage());
 		}
 	}
-	
-	public void disconnect() {
-		try {
-			thread.interrupt();
-			socket.close();
-			in.close();
-			out.close();
-			clientView.sendButton.setEnabled(false);
-			clientView.connectButton.setText("Connect");
-		} catch(UnknownHostException uhe) {
-			clientView.errors.setText(uhe.getMessage());
-		} catch(IOException ioe) {
-			clientView.errors.setText(ioe.getMessage());
-		}
-	}
-	
-	public void send() {
-		out.print(clientView.userInput.getText() + "\r\n");
-		out.flush();
-		clientView.result.append("> " + clientView.userInput.getText() + '\n');
-		clientView.userInput.setText("");
-	}
-	
 	
 	public static void main(String[] args) {
 		Client display = new Client();
@@ -73,20 +75,16 @@ public class Client extends JFrame {
 }
 
 class ReadThread extends Thread {
-	BufferedReader in;
+	Scanner in;
 	JTextArea display;
-	public ReadThread(BufferedReader br, JTextArea jta) {
+	public ReadThread(Scanner br, JTextArea jta) {
 		in = br;
 		display = jta;
 	}
 	public void run() {
 		String s;
-		try {
-			while ((s = in.readLine()) != null) {
-				display.append(s + '\n');
-			}
-		} catch (IOException ioe) {
-			System.out.println("Error reading from socket");
+		while ((s = in.nextLine()) != null) {
+			display.append(s + '\n');
 		}
 	}
 }
