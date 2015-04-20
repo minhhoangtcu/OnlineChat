852eae3459e91a7f057ab3056f3bdf5f3948fdb3
minhhoangtcu <minh.hoang@tcu.edu>
2015-04-20 16:43:56 -0500

    Make the class more readable

--- OnlineChat/src/ClientView.java
+++ OnlineChat/src/ClientView.java
@@ -1,24 +1,23 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
-import java.io.*;
-import java.util.*;
 
 public class ClientView extends JFrame{
-	JTextArea result = new JTextArea();
-	JTextField userInput = new JTextField(20);
-	JTextField tfSever = new JTextField();
-	JTextField tfPort = new JTextField();
-	JButton connectButton = new JButton("Connect");
-	JButton sendButton = new JButton("Send");
-	JLabel errors = new JLabel();
-	JLabel lbSever = new JLabel("Sever");
-	JLabel lblPort = new JLabel("Port");
-	JScrollPane scroller = new JScrollPane();
-	Color purple = new Color (153, 0, 204);
+	JTextArea result;
+	JTextField userInput;
+	JTextField tfSever;
+	JTextField tfPort;
+	JButton connectButton;
+	JButton sendButton;
+	JLabel errors;
+	JLabel lbSever;
+	JLabel lblPort;
+	JScrollPane scroller;
 	JPanel contentPane;
+	final Color PURPLE = new Color (153, 0, 204);
 	
-	public static void main() {
+	public static void main(String[] args) {
 		ClientView view = new ClientView();
 		view.setVisible(true);
 	}
@@ -46,82 +45,83 @@ public class ClientView extends JFrame{
 	private void setUpContentPane() {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setOpaque(true);
 		contentPane.setBackground(new Color(250, 250, 250));
 		contentPane.setLayout(null);
+		setContentPane(contentPane);
 	}
 
 	private void setUpResult() {
-		result.setBorder(BorderFactory.createLineBorder(purple,1));
-		result.addMouseListener(new java.awt.event.MouseAdapter() {
-		    public void mouseEntered(java.awt.event.MouseEvent evt) {
-		        result.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
-		    }
-
-		    public void mouseExited(java.awt.event.MouseEvent evt) {
-		        result.setBorder(BorderFactory.createLineBorder(purple,1));
-		    }
-		});
+		result = new JTextArea();
+		scroller = new JScrollPane();
+		result.setBorder(BorderFactory.createLineBorder(PURPLE,1));
 		scroller.getViewport().add(result);
 		scroller.setBounds(10, 11, 344, 258);
 		contentPane.add(add(scroller));
 	}
 		
 	private void setUpUserInput() {
-		contentPane.add(userInput); 
+		userInput = new JTextField(20);
 		userInput.setBounds(10, 280, 344, 24);
 		userInput.setColumns(10);
-		userInput.setBorder(BorderFactory.createLineBorder(purple,1));
+		userInput.setBorder(BorderFactory.createLineBorder(PURPLE,1));
+		contentPane.add(userInput);
 	}
 	
 	private void setUpSendButton() {
-		contentPane.add(sendButton); 
+		sendButton = new JButton("Send");
 		sendButton.setBounds(364, 281, 113, 23);
 		sendButton.setEnabled(false);
 		sendButton.setContentAreaFilled(false);
-		sendButton.setBackground(purple);
+		sendButton.setBackground(PURPLE);
 		sendButton.setForeground(Color.WHITE);
+		contentPane.add(sendButton); 
 	}
 			
 	private void setUpConnectButton() {
-		contentPane.add(connectButton); 
+		connectButton = new JButton("Connect");
 		connectButton.setBounds(364, 310, 113, 23);
-		connectButton.setBackground(purple);
+		connectButton.setBackground(PURPLE);
 		connectButton.setForeground(Color.WHITE);
+		contentPane.add(connectButton); 
 	}
 
 	private void setUpServerLabel() {
+		lbSever = new JLabel("Sever");
 		lbSever.setFont(new Font("SansSerif", Font.BOLD, 12));
 		lbSever.setBounds(10, 315, 50, 14);
-		lbSever.setForeground(purple);
+		lbSever.setForeground(PURPLE);
 		contentPane.add(lbSever);
 	}
 	
 	private void setUpServerTF() {
+		tfSever = new JTextField();
 		tfSever.setBounds(55, 311, 137, 20);
 		tfSever.setColumns(10);
-		tfSever.setBorder(BorderFactory.createLineBorder(purple,1));
+		tfSever.setBorder(BorderFactory.createLineBorder(PURPLE,1));
 		tfSever.setText("localhost");
 		contentPane.add(tfSever);
 	}
 		
 	private void setUpPortLabel() {
+		lblPort = new JLabel("Port");
 		lblPort.setFont(new Font("SansSerif", Font.BOLD, 12));
 		lblPort.setBounds(230, 313, 46, 14);
-		lblPort.setForeground(purple);
+		lblPort.setForeground(PURPLE);
 		contentPane.add(lblPort);
 	}
 	
 	private void setUpPortTF() {
+		tfPort = new JTextField();
 		tfPort.setBounds(268, 311, 86, 20);
 		tfPort.setColumns(10);
-		tfPort.setBorder(BorderFactory.createLineBorder(purple,1));
+		tfPort.setBorder(BorderFactory.createLineBorder(PURPLE,1));
 		tfPort.setText("6969");
 		contentPane.add(tfPort);
 	}
 	
 	private void setUpError() {
+		errors = new JLabel();
 		contentPane.add(errors);
 	}
 }
