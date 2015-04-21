f6af4d6618e07e0017aefa75c1029543d0eabee8
minhhoangtcu <minh.hoang@tcu.edu>
2015-04-21 16:06:47 -0500

    deleted a junk class

--- OnlineChat/src/ClientControl.java
+++ OnlineChat/src/ClientControl.java
@@ -1,12 +1,9 @@
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.io.IOException;
-import java.net.UnknownHostException;
 
 import javax.swing.BorderFactory;
 
